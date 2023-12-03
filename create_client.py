80383acd5ca6e99385d59eaefbd0b6a83fbdd96a
stephenkfrey <2404105+stephenkfrey@users.noreply.github.com>
2023-12-02 19:52:03 -0800

    Create create_client.py

---
+++ create_client.py
@@ -0,0 +1,24 @@
+### Sqlite3 replacement for Chroma ### 
+import os
+import sys
+import pysqlite3
+sys.modules['sqlite3'] = pysqlite3
+### ###
+# 
+# import chromadb
+import chromadb
+from chromadb.utils import embedding_functions
+
+# create client 
+chroma_client = chromadb.PersistentClient(path="chromadb")
+
+# create embed function 
+qa_embed_fun = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="multi-qa-MiniLM-L6-cos-v1")
+
+# create collection
+collection = chroma_client.create_collection(
+        name="imagecoll",
+        metadata={"hnsw:space": "cosine"}, # l2 is the default
+        embedding_function=qa_embed_fun
+)
+
