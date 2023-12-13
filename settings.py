dd8368f24093e44d96fef103f237bc7a905a7b01
stephenkfrey <2404105+stephenkfrey@users.noreply.github.com>
2023-12-12 22:49:20 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,11 +4,12 @@
 # these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
 __import__('pysqlite3')
 import sys
+import os
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
